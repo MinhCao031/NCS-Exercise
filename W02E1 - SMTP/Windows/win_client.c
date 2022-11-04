54d2fdc617523153a2ca4bc79c7c6113f4cb03e3
Minh Cao <19020063@vnu.edu.vn>
2022-11-04 17:14:15 +0700

    Update win_client.c

--- W02E1 - SMTP/Windows/win_client.c
+++ W02E1 - SMTP/Windows/win_client.c
@@ -1,133 +1,99 @@
-#include <winsock2.h>
 #include <stdio.h>
+#include <strings.h>
+#include <winsock2.h>
 
-#define CLIENT_OFFLINE "You can Quit now."
+#define MAX_BYTE 1048576
 
-char szServerIPAddr[20] = "127.0.0.1";   // Put here the IP address of the server
-int nServerPort = 8081;                    // The server port that will be used by                                            // clients to talk with the server
+// Put here the IP address of the server
+const char szServerIPAddr[20] = "192.168.12.51";
+//const char szServerIPAddr[20] = "192.168.12.108"
 
-void* doReceiving(void* sockID);
-BOOL InitWinSock2_0();
+// The server port that will be used by clients to talk with the server
+const int nServerPort = 25; 
 
-int main()
-{
-    //printf("Enter the server IP Address: ");
-    //scanf("%s", szServerIPAddr);
-    //printf("Enter the server port number: ");
-    //scanf("%d", nServerPort);
-    printf("\n%s\t%d\n", szServerIPAddr, nServerPort);
+BOOL InitWinSock2_0() {
+  WSADATA wsaData;
+  WORD wVersion = MAKEWORD(2, 0);
+  if (!WSAStartup(wVersion, &wsaData))
+    return TRUE;
+  return FALSE;
+}
 
-    if (!InitWinSock2_0()) {
-        printf("Unable to Initialize Windows Socket environment, ERRORCODE = %d\n", WSAGetLastError());
-        return -11;
-    }
+int main(/*int argc, char* argv[]*/) {
+  if (!InitWinSock2_0()) {
+    printf("Unable to Initialize Windows Socket environment, ERRORCODE = %d\n",
+      WSAGetLastError());
+    return -11;
+  }
 
-    SOCKET hClientSocket;
-    hClientSocket = socket(
-        AF_INET,        // The address family. AF_INET specifies TCP/IP
-        SOCK_STREAM,    // Protocol type. SOCK_STREM specified TCP
-        0               // Protoco Name. Should be 0 for AF_INET address family
-    );
-    printf("%d\n", hClientSocket);
-    if (hClientSocket == INVALID_SOCKET) {
-        printf("Unable to create Server socket\n");
-        // Cleanup the environment initialized by WSAStartup()
-        WSACleanup();
-        return -12;
-    }
+  SOCKET hClientSocket;
+  hClientSocket = socket(
+    AF_INET,    // The address family. AF_INET specifies TCP/IP
+    SOCK_STREAM,// Protocol type. SOCK_STREM specified TCP
+    0           // Protoco Name. Should be 0 for AF_INET address family
+  );
 
-    // Create the structure describing various Server parameters
-    struct sockaddr_in serverAddr;
+  if (hClientSocket == INVALID_SOCKET) {
+    printf("Unable to create Server socket\n");
+    // Cleanup the environment initialized by WSAStartup()
+    WSACleanup();
+    return -12;
+  }
 
-    serverAddr.sin_family = AF_INET;     // The address family. MUST be AF_INET
-    serverAddr.sin_addr.s_addr = inet_addr(szServerIPAddr);
-    serverAddr.sin_port = htons(nServerPort);
+  //print some info
+  printf("Server's IP Address: %s\n", szServerIPAddr);
+  printf("Server's port:   %d\n", nServerPort);
+  printf("Socket ID:       %d\n", hClientSocket);
 
-    // Connect to the server
-    if (connect(hClientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
-        printf("Unable to connect to %s on port %d\n", szServerIPAddr, nServerPort);
-        closesocket(hClientSocket);
-        WSACleanup();
-        return -13;
-    }
+  // Create the structure describing various Server parameters
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET; // The address family. MUST be AF_INET
+  serverAddr.sin_addr.s_addr = inet_addr(szServerIPAddr);
+  serverAddr.sin_port = htons(nServerPort);
 
-    HANDLE chatThread;
-    DWORD dwThreadId;
-    chatThread = CreateThread(NULL, 0,
-        (void*) doReceiving,
-        (void*) &hClientSocket,
-        0, &dwThreadId
-    );
-
-    printf("Connection established ............\n");
-
-    BOOL closed = FALSE;
-    while (0==0) {
-        char *szBuffer = malloc(1024);
-		scanf("%s", szBuffer);
-		send(hClientSocket,szBuffer,1024,0);
-
-		if (strcmp(szBuffer,"LIST") == 0) {
-			//send(hClientSocket,szBuffer,1024,0);
-		}
-		if (strcmp(szBuffer,"EHLO") == 0) {
-			//send(hClientSocket,szBuffer,1024,0);
-		}
-		if (strcmp(szBuffer,"MAIL") == 0) {
-			//send(hClientSocket,szBuffer,1024,0);
-			// Expected "FROM: "
-			scanf("%s",szBuffer);
-			send(hClientSocket,szBuffer,1024,0);
-			// Expected ~"bob@gmail.com"
-			scanf("%[^\n]s",szBuffer);
-			while(*szBuffer == ' ') szBuffer++;
-			send(hClientSocket,szBuffer,1024,0);
-		}
-		if (strcmp(szBuffer,"RCPT") == 0) {
-			//send(hClientSocket,szBuffer,1024,0);
-			// Expected "TO: "
-			scanf("%s",szBuffer);
-			send(hClientSocket,szBuffer,1024,0);
-			// Expected ~"alice@yahoo.com"
-			scanf("%[^\n]s",szBuffer);
-			while(*szBuffer == ' ') szBuffer++;
-			send(hClientSocket,szBuffer,1024,0);
-		}
-		if (strcmp(szBuffer,"DATA") == 0) {
-			// change sending to TRUE
-			// send(hClientSocket,szBuffer,1024,0);
-		}
-		if (strcmp(szBuffer,"QUIT") == 0) {
-            send(hClientSocket,"QUIT",1024,0);
-            recv(hClientSocket,szBuffer,1024,0);
-            if (strcmp(szBuffer,CLIENT_OFFLINE) == 0) break;
-		}
-    }
-    if (!closed) send(hClientSocket,"QUIT",1024,0);
+  // Connect to the server
+  if (connect(hClientSocket, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) < 0) {
+    printf("Unable to connect to %s on port %d\n", szServerIPAddr, nServerPort);
     closesocket(hClientSocket);
     WSACleanup();
-    return 0;
-}
+    return -13;
+  }
 
-BOOL InitWinSock2_0()
-{
-    WSADATA wsaData;
-    WORD wVersion = MAKEWORD(2, 0);
-    if (!WSAStartup(wVersion, &wsaData))
-        return TRUE;
-    return FALSE;
-}
+  printf("Connection established ............\n\n");
+  char *strRecv = malloc(MAX_BYTE);
+  int check = -1;
 
+  // Step 1: First signal from hmail server
+  recv(hClientSocket, strRecv, MAX_BYTE, 0);
+  printf("Received: %s\n", strRecv);
+  if (strstr(strRecv, "220") == NULL) {
+    return 220;
+  }
 
-void* doReceiving(void* hClientSocket) {
-	int clientSocket = *((int*) hClientSocket);
+  for (int i = 1; i < 100; i++) {
+    // Input command
+    printf("Loop %d, input: ", i);
+    fgets(strRecv, MAX_BYTE, stdin);
+    strRecv[strlen(strRecv)-1] = '\0';
+    // strRecv[strlen(strRecv)-1] = '\r';
+    // sprintf(strRecv, "%s\n", strRecv);
 
-	while(1) {
-		char data[1024];
-		int read = recv(clientSocket,data,1024,0);
-		data[read] = '\0';
-        // if (strcmp(data, CLIENT_OFFLINE)) return;
-		printf("%s\n",data);
-		for (int k = 0; k < 1024; k++) data[k] = '\0';
-	}
+    printf("You typed: $$$%s&&&\n", strRecv);
+    check = send(hClientSocket, strRecv, MAX_BYTE, 0);
+
+    //printf("check send: %d\n", check);
+    //printf("Sent: %s\n", strRecv);
+    char data[MAX_BYTE];
+    check = recv(hClientSocket, data, MAX_BYTE, 0);
+    //printf("check recv: %d\n", check);
+    printf("Received: %s\n", data);
+    for (int j = 0; j < MAX_BYTE; j++) {
+      data[j] = '\0';
+      strRecv[i] = '\0';
+    }
+  }
+
+  closesocket(hClientSocket);
+  WSACleanup();
+  return 0;
 }
